924aae0990fa7b5433f259f0a31715995739c68f
SATYAJIT-2004 <satyajit2568@gmail.com>
2026-02-02 16:24:53 +0530

    Update registration API

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ import User from "../models/User.js";
 import generateToken from "../utils/generateToken.js";
 
 export const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password, role,address } = req.body;
+  const { name, email, password, role } = req.body;
 
   const userExists = await User.findOne({ email });
   if (userExists) {
@@ -11,7 +11,7 @@ export const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User already exists");
   }
 
-  const user = await User.create({ name, email, password, role,address });
+  const user = await User.create({ name, email, password, role});
 
   res.status(201).json({
     message: "User registered successfully",
